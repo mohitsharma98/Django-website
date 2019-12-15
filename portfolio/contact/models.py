b3e5cdd3c4dbf8948ea954cc4b69d79a6e4cec62
mohitsharma98 <mohitsharma30898@gmail.com>
2019-12-15 13:58:32 +0530

    Updated contacts module

--- portfolio/contact/models.py
+++ portfolio/contact/models.py
@@ -11,8 +11,11 @@ class Contact(models.Model):
         return self.name
 
 class Information(models.Model):
-    address = models.TextField(max_length=200)
-    phone = models.TextField(max_length=200)
+    addressline1 = models.TextField(max_length=100)
+    addressline2 = models.TextField(max_length=50)
+    addressline3 = models.TextField(max_length=50)
+    phone = models.TextField(max_length=50)
+    time = models.TextField(max_length=50)
 
     def __str__(self):
-        return self.address
+        return self.addressline1
